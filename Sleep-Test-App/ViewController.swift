dffdca0ee4defdea5b7ff6107dac60b01eff1383
Nikita <nsdidenko.developer@gmail.com>
2021-08-05 14:14:41 +0300

    iOS 13, clean up mess

--- Sleep-Test-App/ViewController.swift
+++ Sleep-Test-App/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Sleep-Test-App
-//
-//  Created by Nikita on 5.08.21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
